deba54cbbec843cd587a8a2132f148eda22b8558
rojaborangokhan <70016968+rojaborangokhan@users.noreply.github.com>
2022-07-08 15:34:08 +0300

    board design almost completed

--- Assets/Scripts/BoardCreation.cs
+++ Assets/Scripts/BoardCreation.cs
@@ -8,6 +8,7 @@ public class BoardCreation : MonoBehaviour
     [SerializeField] private int _boardWidth, _boardHeight;
     [SerializeField] private GameObject _cube;
     [SerializeField] private Tile _tile;
+    private GameObject spawnedTile;
     private void Awake()
     {
         GenerateGrid(_boardWidth, _boardHeight);
@@ -19,13 +20,18 @@ public class BoardCreation : MonoBehaviour
         {
             for (int j = 0; j < boardWidth; j++)
             {
-                var spawnedTile = Instantiate(_cube, new Vector3(j,0, i), Quaternion.identity);
-                spawnedTile.name = $"Tile {j} {i}";
+                spawnedTile = Instantiate(_cube, new Vector3(j,0, i), Quaternion.identity);
+                spawnedTile.name = $"{j} {i}";
+                if ((i ==0 && j <= boardWidth-1) || (j<=boardWidth-1 && i==boardHeight-1))
+                {
+                    Debug.Log(spawnedTile.name);
+                }
                 var isOffset = (i % 2 == 0 && j % 2 != 0) || (i % 2 != 0 && j % 2 == 0);
-                Debug.Log(isOffset);
                 _tile.InIt(isOffset);
+                
             }
         }
+        
     }
     
     
