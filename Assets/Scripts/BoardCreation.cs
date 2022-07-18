e2779f148295079e5772384ca1c2a919f74cd9cd
rojaborangokhan <70016968+rojaborangokhan@users.noreply.github.com>
2022-07-18 14:59:10 +0300

    new script added

--- Assets/Scripts/BoardCreation.cs
+++ Assets/Scripts/BoardCreation.cs
@@ -9,6 +9,9 @@ public class BoardCreation : MonoBehaviour
     [SerializeField] private GameObject _cube;
     [SerializeField] private Tile _tile;
     private GameObject spawnedTile;
+    [SerializeField] private GameObject _rock;
+    [SerializeField] private GameObject _paper;
+    [SerializeField] private GameObject _scissor;
     private void Awake()
     {
         GenerateGrid(_boardWidth, _boardHeight);
@@ -25,6 +28,8 @@ public class BoardCreation : MonoBehaviour
                 if ((i ==0 && j <= boardWidth-1) || (j<=boardWidth-1 && i==boardHeight-1))
                 {
                     Debug.Log(spawnedTile.name);
+                    // ilk sıraya buradan ulaşıyoruz. Bütün taşlar ilk sıradan çıktıktan sonra bu sıra kullanılamayacak.
+                    
                 }
                 var isOffset = (i % 2 == 0 && j % 2 != 0) || (i % 2 != 0 && j % 2 == 0);
                 _tile.InIt(isOffset);
---
+++ Assets/Scripts/ObjectSpawning.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawning : MonoBehaviour
+{
+    [SerializeField] private GameObject _paper;
+    [SerializeField] private GameObject _rock;
+    [SerializeField] private GameObject _scissor;
+
+    private void Awake()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
